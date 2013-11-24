11469bea09b920cfdd7d721ef2add6508784ffca
Tim <nighttrain0486@yahoo.com>
2013-11-24 17:33:13 -0500

    final version

--- analyzer.rb
+++ analyzer.rb
@@ -12,3 +12,12 @@ puts "#{total_characters_nospaces} characters excluding spaces"
 
 word_count = text.split.length
 puts "#{word_count} words"
+
+sentence_count = text.split(/\.|\?|\!/).length
+puts "#{sentence_count} sentences."
+
+paragraph_count = text.split(/\n\n/).length
+puts "#{paragraph_count} paragraphs."
+
+puts "#{sentence_count / paragraph_count} sentences per paragraph (average)"
+puts "#{word_count / sentence_count} words per sentence (average)"
